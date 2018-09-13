e9d26a154027e375792c62f1832f68b38c34fcd9
MrBBot <me@mrbbot.co.uk>
2018-09-13 13:37:11 +0100

    Implemented basic shepherd functions

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
+const rimraf = require("rimraf");
+const childProcess = require("child_process");
 
 const unzip = require("./unzip");
 
@@ -11,16 +13,68 @@ const upload = multer({ storage: multer.memoryStorage() });
 const outPath = path.join(__dirname, "out");
 !fs.existsSync(outPath) && fs.mkdirSync(outPath);
 
+app.get("/", (_req, res) => res.sendFile(path.join(__dirname, "index.txt")));
+
+let child = {
+  process: null,
+  log: ""
+};
+
+const logData = data => (child.log += data);
+
+const killProcess = () => child.process && child.process.kill("SIGINT");
+
 app.post("/upload/upload", upload.single("uploaded_file"), (req, res) => {
-  unzip.unzipBuffer(req.file.buffer, outPath, err => {
+  killProcess();
+  child.log = "";
+
+  const respond = err => {
     if (err) console.log(err);
     res.status(err ? 500 : 204);
     res.send(err);
+  };
+
+  rimraf(outPath, err => {
+    err
+      ? respond(err)
+      : fs.mkdir(outPath, err => {
+          err
+            ? respond(err)
+            : req.file.mimetype === "application/zip"
+              ? unzip.unzipBuffer(req.file.buffer, outPath, respond)
+              : fs.writeFile(
+                  path.join(outPath, "main.py"),
+                  req.file.buffer,
+                  respond
+                );
+        });
   });
 });
 
+app.post("/run/run", (_req, res) => {
+  killProcess();
+  child.log = "";
+
+  child.process = childProcess.spawn("python", [
+    "-u",
+    path.join(outPath, "main.py")
+  ]);
+  child.process.stdout.on("data", logData);
+  child.process.stderr.on("data", logData);
+  child.process.on("close", () => (child.process = null));
+
+  res.sendStatus(204);
+});
+
+app.post("/run/stop", (_req, res) => {
+  killProcess();
+  child.process = null;
+
+  res.sendStatus(204);
+});
+
 app.get("/run/output", (_req, res) => {
-  res.send("Logs!");
+  res.send(child.log);
 });
 
 const port = parseInt(process.env.SHEPHERD_PORT) || 4000;
