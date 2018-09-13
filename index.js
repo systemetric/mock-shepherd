53725d7a2042e49fe1d096d15bb23c9b17553c11
MrBBot <me@mrbbot.co.uk>
2018-09-13 13:48:06 +0100

    Added logging

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const app = express();
 const upload = multer({ storage: multer.memoryStorage() });
 
 const outPath = path.join(__dirname, "out");
-!fs.existsSync(outPath) && fs.mkdirSync(outPath);
 
 app.get("/", (_req, res) => res.sendFile(path.join(__dirname, "index.txt")));
 
@@ -20,19 +19,29 @@ let child = {
   log: ""
 };
 
-const logData = data => (child.log += data);
+const logData = data => {
+  child.log += data;
+  console.log(`[LOG] ${data.toString().trim()}`);
+};
 
-const killProcess = () => child.process && child.process.kill("SIGINT");
+const killProcess = () => {
+  if (child.process) {
+    child.process.kill("SIGINT");
+    console.log("[INFO] Killed process!");
+  }
+};
 
 app.post("/upload/upload", upload.single("uploaded_file"), (req, res) => {
   killProcess();
   child.log = "";
 
   const respond = err => {
-    if (err) console.log(err);
+    console.log(err ? `[ERROR] ${err}` : "[INFO] Uploaded!");
     res.status(err ? 500 : 204);
     res.send(err);
   };
+
+  console.log(`[INFO] Uploading ${req.file.originalname}...`);
 
   rimraf(outPath, err => {
     err
@@ -52,21 +61,29 @@ app.post("/upload/upload", upload.single("uploaded_file"), (req, res) => {
 });
 
 app.post("/run/run", (_req, res) => {
+  console.log("[INFO] Received run request!");
+
   killProcess();
   child.log = "";
 
+  console.log("[INFO] Spawning process...");
   child.process = childProcess.spawn("python", [
     "-u",
     path.join(outPath, "main.py")
   ]);
   child.process.stdout.on("data", logData);
   child.process.stderr.on("data", logData);
-  child.process.on("close", () => (child.process = null));
+  child.process.on("close", () => {
+    child.process = null;
+    console.log("[INFO] Process terminated!");
+  });
 
   res.sendStatus(204);
 });
 
 app.post("/run/stop", (_req, res) => {
+  console.log("[INFO] Received stop request!");
+
   killProcess();
   child.process = null;
 
